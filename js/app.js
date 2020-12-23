ba12ddcbd965862d40d019a073033538093dd69d
Ariful Islam <76473499+Armansays@users.noreply.github.com>
2020-12-23 02:24:35 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+document.getElementById('inputVel').addEventListener('keypress',function(){
+   
+    var vel0 = document.getElementById('inputVel').value;
+
+    document.getElementById('output').srcdoc = vel0
+    
+
+})
+
+
